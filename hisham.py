cb9038d35084897527d5f5d69743f1e727915ea1
Atif-Xiddi <56693128+Atif249@users.noreply.github.com>
2024-01-03 21:57:05 +0500

    Add files via upload
     ----- TERMUX RUN CODE -----
    
    apt update
    
    apt upgrade 
    
    apt install git 
    
    git clone https://github.com/Atif249/Hisham.py.git
    
    cd hisham.py
    
    python hisham.py

---
+++ hisham.py
@@ -0,0 +1,96 @@
+import os 
+
+def A_atif__Hisham():
+     
+     print()
+     
+     print(""" +-+-+-+-+-+-+
+ |H|I|S|H|A|M|
+ +-+-+-+-+-+-+
+ 
+ """)
+     print("-------------------------")
+     print("""H  °  I  °  S  °  H  °  M  """)
+     print("-------------------------")
+     print()
+     
+     
+     print("------- MY CV ----------")
+     print()
+     print("[~]  -----------  HISHAM")
+     print()
+     print("[~]  -----------   HISHI")
+     print()
+     print("[~]  -----   315 9990622")
+     print()
+     print("[~]  ------------- ALONE ")
+     print()
+     print("[~]  ------------ HACKER")
+     print()
+     print("-------------------------")
+
+print("-------------------------")
+a = input("ENTER USER NAME : ")
+print()
+b= input("ENTER PASSWORD : ")
+print("-------------------------")
+
+if a=='Atif':
+     A_atif__Hisham()
+     
+elif a=='Hisham':
+     A_atif__Hisham()
+     
+else:
+     print("WRONG USER NAME !!!")
+     
+     
+#A_atif__Hisham()
+
+
+
+def tur():
+     turtle.fillcolor("pink")
+     turtle.begin_fill()
+     turtle.circle(10, 180)
+     turtle.circle(25, 110)
+     turtle.left(50)
+     turtle.circle(60, 45)
+     turtle.circle(20, 170)
+     turtle.right(24)
+     turtle.fd(30)
+     turtle.left(10)
+     turtle.circle(30, 110)
+     turtle.fd(20)
+     turtle.left(40)
+     turtle.circle(90, 70)
+     turtle.circle(30, 150)
+     turtle.right(30)
+     turtle.fd(15)
+     turtle.circle(80, 90)
+     turtle.left(15)
+     turtle.fd(45)
+     turtle.right(165)
+     turtle.fd(20)
+     turtle.left(155)
+     turtle.circle(150, 80)
+     turtle.left(50)
+     turtle.circle(150, 90)
+     turtle.end_fill()
+
+# Petal 1
+     turtle.left(150) 
+     turtle.circle(-90, 70)
+     turtle.left(20)
+     turtle.circle(75, 105)
+     turtle.setheading(60)
+     turtle.circle(80, 98)
+     turtle.circle(-90, 40)
+
+# Petal 2
+     turtle.left(180)
+     turtle.circle(90, 40)
+     turtle.circle(-80, 98)
+     turtle.setheading(-83 )
+    
+    
